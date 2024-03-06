2e915874ea1cb5e110cb29c0d537bc53ed1fdad9
James Bouchat <80969731+heidelbergah@users.noreply.github.com>
2024-03-06 12:31:55 -0500

    Added after/plugin files

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,24 @@
+local lsp = require('lsp-zero').preset({})
+local lspconfig = require('lspconfig')
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+--require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+--require('lspconfig').pyright.setup(lsp.pyright())
+lspconfig.jedi_language_server.setup{}
+lspconfig.clangd.setup{}
+lspconfig.lua_ls.setup{}
+
+local servers = { 'clangd' }
+for _, lsp in pairs(servers) do
+   require('lspconfig')[lsp].setup {
+      on_attach = on_attach,
+      flags = {
+         debounce_text_changes = 150,
+      }
+   }
+end
+
+lsp.setup()
